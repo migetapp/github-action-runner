9adbd8a49de7fd4646fc70a1141c5eb08607b2e3
Krzysztof Taraszka <krzysztof.taraszka@ippnet.pl>
2024-08-26 20:43:44 +0200

    New variable: RUNNER_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "alias docker=podman" >> ~/.bashrc
-source ~/.bashrc
-./config.sh --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --unattended --replace
+./config.sh --name ${RUNNER_NAME} --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --unattended --replace
 ./run.sh
