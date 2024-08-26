013c79b2eb63bc18dc976692b7d7c62de0880ecf
Krzysztof Taraszka <krzysztof.taraszka@ippnet.pl>
2024-08-26 22:42:16 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,12 @@ RUNNER_VOLUME_DIR="/home/podman/runner"
 RUNNER_INSTALL_DIR="/home/podman/actions-runner"
 CONFIG_FILE="$RUNNER_VOLUME_DIR/.runner"
 
-if [ -z "$(ls -A $RUNNER_VOLUME_DIR)" ]; then
-  echo "Runner volume is empty. Copying data to volume..."
-  cp -r $RUNNER_INSTALL_DIR/* $RUNNER_VOLUME_DIR/
-fi
-
-cd $RUNNER_VOLUME_DIR
-
-sleep 100000000
-
 if [ ! -f "$CONFIG_FILE" ]; then
-  echo "Runner is not configured. Configuring..."
+  echo "Runner is not initialized. Copying data to volume..."
+  cp -r $RUNNER_INSTALL_DIR/* $RUNNER_VOLUME_DIR/
+
+  echo "Configuring the GitHub runner..."
+  cd $RUNNER_VOLUME_DIR
   ./config.sh --unattended --replace --name ${RUNNER_NAME} --url https://github.com/${GITHUB_REPOSITORY} --token $RUNNER_TOKEN
 else
   echo "Runner is already configured. Skipping configuration."
